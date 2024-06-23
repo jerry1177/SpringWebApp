fca0ff8b0dcba36102bf4e9f54f86692d79f4e09
jerry1177 <jerrymaldonadom@gmail.com>
2024-06-23 11:00:26 -0700

    Prepared project folders and added maven dependencies.

--- src/main/java/com/maldonadopublisher/springwebapp/repositories/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maldonadopublisher.springwebapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.maldonadopublisher.springwebapp.model.Author;
-
-public interface AuthorRepository extends CrudRepository<Author, Long>{
-    
-}
--- src/main/java/com/maldonadopublisher/springwebapp/repositories/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maldonadopublisher.springwebapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.maldonadopublisher.springwebapp.model.Publisher;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-    
-}
