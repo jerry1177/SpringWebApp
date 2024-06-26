c7f7ea13eb72974ca1865c24a7ef7c413e1d23fd
jerry1177 <jerrymaldonadom@gmail.com>
2024-06-25 20:38:38 -0700

    Added owner and vet controllers and views.

--- pet-clinic-web/src/main/java/com/maldonadopublisher/petclinicweb/SpringwebappApplication.java
+++ pet-clinic-web/src/main/java/com/maldonadopublisher/petclinicweb/SpringwebappApplication.java
@@ -1,4 +1,4 @@
-package com.maldonadopublisher;
+package com.maldonadopublisher.petclinicweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
